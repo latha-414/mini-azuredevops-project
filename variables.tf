2fbf026fbc184f5d7ea4a1a2e3dbe135b3b91e62
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-28 14:50:03 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "resource_group_name" {
 variable "location" {
   description = "Azure region"
   type        = string
-  default     = "East US"
+  default     = "Central India"
 }
 
 variable "app_service_plan_name" {
